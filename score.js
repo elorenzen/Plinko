4f1214abb452f08f816babd72acf50c91143d327
Eric <eric@kellyklee.com>
2019-10-21 11:49:18 -0600

    Create function to split test, training data, add helper function

--- score.js
+++ score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,13 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-    const bucket =_.chain(outputs)
-        .map( row => [distance(row[0]), row[3]] )
+    const bucket = console.log(`The ball will fall into bucket #${bucket}`)
+}
+
+// Helper function
+function knn(data, point) {
+    return _.chain(data)
+        .map( row => [distance(row[0], point), row[3]] )
         .sortBy( row => row[0] )
         .slice(0, k)
         .countBy( row => row[1] )
@@ -18,10 +22,18 @@ function runAnalysis() {
         .first()
         .parseInt()
         .value();
-
-    console.log(`The ball will fall into bucket #${bucket}`)
 }
 
-function distance(point) {
-	return Math.abs(point - predictionPoint)
+function distance(pointA, pointB) {
+	return Math.abs(pointA - pointB)
+}
+
+// Differentiates training vs. test data
+function splitDataset(data, testCount) {
+    const shuffled = _.shuffled(data);
+
+    const testSet = _.slice(shuffled, 0, testCount);
+    const trainingSet = _.slice(shuffled, testCount)
+
+    return [testSet, trainingSet];
 }
